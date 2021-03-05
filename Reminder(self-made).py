f633611e4c1dc7d7f8150800e6de2646455e022e
KumikoNyan <79185676+KumikoNyan@users.noreply.github.com>
2021-03-05 14:29:51 +0800

    +Working

--- Reminder(self-made).py
+++ Reminder(self-made).py
@@ -1,11 +1,11 @@
+# working SHISTIsadhjjkshdjkashdjk
 import discord
 import asyncio
-import time
+import datetime
 from discord.ext import commands
-from datetime import datetime
-
 
 intents = discord.Intents.default()
+intents.members = True
 client = commands.Bot(command_prefix='!', intents=intents)
 token = ''
 
@@ -15,18 +15,21 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
 
-@client.command(case_insensitive = True, aliases = ["remind", "remindme", "remind_me"])
 
-@commands.bot_has_permissions(attach_files = True, embed_links = True)
+@client.command(case_insensitive=True, aliases=["remind", "remindme", "remind_me"])
+@commands.bot_has_permissions(attach_files=True, embed_links=True)
 async def reminder(ctx, time, *, reminder):
     print(time)
     print(reminder)
     user = ctx.message.author
     embed = discord.Embed(color=0x55a7f7, timestamp=datetime.datetime.utcnow())
-    embed.set_footer(text="If you have any questions, suggestions or bug reports, please join our support Discord Server: link hidden", icon_url=f"{client.user.avatar_url}")
+    embed.set_footer(
+        text="If you have any questions, suggestions or bug reports, please join our support Discord Server: link hidden",
+        icon_url=f"{client.user.avatar_url}")
     seconds = 0
     if reminder is None:
-        embed.add_field(name='Warning', value='Please specify what do you want me to remind you about.') # Error message
+        embed.add_field(name='Warning',
+                        value='Please specify what do you want me to remind you about.')  # Error message
     if time.lower().endswith("d"):
         seconds += int(time[:-1]) * 60 * 60 * 24
         counter = f"{seconds // 60 // 60 // 24} days"
@@ -42,7 +45,7 @@ async def reminder(ctx, time, *, reminder):
     if seconds == 0:
         embed.add_field(name='Warning',
                         value='Please specify a proper duration, send `reminder_help` for more information.')
-    elif seconds < 300:
+    elif seconds < 30:
         embed.add_field(name='Warning',
                         value='You have specified a too short duration!\nMinimum duration is 5 minutes.')
     elif seconds > 7776000:
@@ -50,7 +53,7 @@ async def reminder(ctx, time, *, reminder):
     else:
         await ctx.send(f"Alright, I will remind you about {reminder} in {counter}.")
         await asyncio.sleep(seconds)
-        await ctx.send(f"Hi, you asked me to remind you about {reminder} {counter} ago.")
+        await ctx.send(f"Hi, you asked me to remind you about {reminder} {counter} ago.", mention_author=True)  # this fucking mention doesn't work yet? cog it to __main__
         return
     await ctx.send(embed=embed)
 
